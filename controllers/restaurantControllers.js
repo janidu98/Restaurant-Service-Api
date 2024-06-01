3fe411d0c55a806e19c68e60f3c707112d1a0139
janidu98 <janinduchathushan@gmail.com>
2024-06-01 19:19:18 +0530

    added update restaurant route

--- controllers/restaurantControllers.js
+++ controllers/restaurantControllers.js
@@ -1,25 +1,72 @@
 import Restaurant from "../model/restaurantModel.js";
 import { HttpException } from "../utils/error.js";
 
-export const createNewRestaurant = async(req, res, next) => {
-    const {name, address, telephone} = req.body;
+//create new restaurant
+export const createNewRestaurant = async (req, res, next) => {
+  const { name, address, telephone } = req.body;
 
-    try {
-        //check all fields are filled or not
-        if(!name || !address || !telephone) {
-            return next(HttpException(400, 'All fields are required!'));
-        }
-
-        const newRestaurant = new Restaurant({
-            name,
-            address,
-            telephone
-        })
-
-        await newRestaurant.save();
-        res.status(201).json({data: newRestaurant, message: 'New Restaurant created successfully'});
-
-    } catch (error) {
-        next(error);
+  try {
+    //check all fields are filled or not
+    if (!name || !address || !telephone) {
+      return next(HttpException(400, "All fields are required!"));
     }
-}
+
+    const newRestaurant = new Restaurant({
+      name,
+      address,
+      telephone,
+    });
+
+    await newRestaurant.save();
+    res
+      .status(201)
+      .json({
+        data: newRestaurant,
+        message: "New Restaurant created successfully",
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//get one restaurant details
+export const getOneRestaurant = async (req, res, next) => {
+  try {
+    //find the restaurant by id
+    const restaurant = await Restaurant.findById(req.params.id);
+
+    //if id is not found
+    if (!restaurant) {
+      return next(HttpException(404, "Restaurant is not found!"));
+    }
+
+    res.status(200).json(restaurant);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//update a restaurant details
+export const updateRestaurant = async (req, res, next) => {
+  try {
+    //find restaurant needed to update using by id
+    const restaurant = await Restaurant.findById(req.params.id);
+
+    //if id is not found
+    if (!restaurant) {
+      return next(HttpException(404, "Restaurant is not found!"));
+    }
+
+    //update restaurant details
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedRestaurant);
+
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { createNewRestaurant } from '../controllers/restaurantControllers.js';
+import { createNewRestaurant, getOneRestaurant, updateRestaurant } from '../controllers/restaurantControllers.js';
 
 const router = express.Router();
 
 //create new restaurant
 router.post('/create', createNewRestaurant);
+
+//get one restaurant details
+router.get('/:id', getOneRestaurant);
+
+//update a restaurant details
+router.post('/update/:id', updateRestaurant);
 
 export default router;
